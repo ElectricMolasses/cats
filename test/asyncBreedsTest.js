9c3f5b1a61a118e2eaf6c29a422b977a0b50be07
Robert Beckett <robert.beckett@onion.red>
2019-10-22 00:37:15 +0000

    Implemented unit test for asynchronous function.

--- test/asyncBreedsTest.js
+++ test/asyncBreedsTest.js
@@ -2,7 +2,12 @@ const { assert } = require('chai');
 const breedDetailsFromFile = require('../asyncBreed');
 
 describe('#breedDetailsFromFile', () => {
-  it('provides via callback, breed details for the Bombay breed', () => {
-    console.log(breedDetailsFromFile);
+  it('provides via callback, breed details for the Bombay breed', (done) => {
+    const expected = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+
+    breedDetailsFromFile('Bombay', (bombay) => {
+      assert.equal(expected, bombay);
+      done();
+    });
   });
 });
