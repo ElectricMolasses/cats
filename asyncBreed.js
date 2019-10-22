19502804ccdc51ed59fd77e9dfc81da7d30273ca
Robert Beckett <robert.beckett@onion.red>
2019-10-22 00:40:43 +0000

    Corrected bugwhere the callback hangs if there's an error.  Implemented unit test on file not existing.

--- asyncBreed.js
+++ asyncBreed.js
@@ -6,6 +6,7 @@ const breedDetailsFromFile = function(breed, callback) {
     // ISSUE: Returning from inner callback function, not our main function.
     console.log('Callback: I have the data!');
     if (!error) callback(data);
+    else callback(undefined);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so this function returns undefined.
--- test/asyncBreedsTest.js
+++ test/asyncBreedsTest.js
@@ -10,4 +10,11 @@ describe('#breedDetailsFromFile', () => {
       done();
     });
   });
+
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+      assert.equal(undefined, desc);
+      done();
+    });
+  });
 });
